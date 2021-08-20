f24ac57ba9a8eb6eb00bc0b9dd5b176954979795
Luis Carlos Rodriguez Barrios <luiscarrodba97@gmail.com>
2021-08-19 22:19:50 -0500

    Created first version of Product model

--- products_ms/products/models.py
+++ products_ms/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    code = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_lenght=128  , unique=True)
+    description = models.CharField(max_length=256)
+    price = models.IntegerField()
+    stock = models.IntegerField()
