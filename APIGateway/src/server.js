ab0b8b59f951be2cf9edf84191c06a9efdf09b3b
Camilo Navarro <camilo.navarro9812@gmail.com>
2021-08-21 17:17:30 -0500

    Files APIGateway

---
+++ APIGateway/src/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    users_api_url: process.env.USERS_API_URL || 'https://mision-tic-users5.herokuapp.com/',
+    account_api_url: process.env.ACCOUNT_API_URL || 'https://cuentasmstest.herokuapp.com/',
+    
+};
