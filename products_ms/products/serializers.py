c505f4f66aa4ce5d4cd61fa552ae4a3246b3b22f
Luis Carlos Rodriguez Barrios <luiscarrodba97@gmail.com>
2021-08-22 21:09:04 -0500

    Refactored to use viewsets and routers and included custom action to reduce product stock

--- products_ms/products/serializers.py
+++ products_ms/products/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Product
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
