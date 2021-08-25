ef2b465a4f5dac98dc293a4d355e1438a12aec3b
Luis Carlos Rodriguez Barrios <luiscarrodba97@gmail.com>
2021-08-25 16:54:14 -0500

    id included in product serialization

--- products_ms/products/models.py
+++ products_ms/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    code = models.BigAutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=128  , unique=True)
     description = models.CharField(max_length=256)
     price = models.IntegerField()
