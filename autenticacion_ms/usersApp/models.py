4a5dea3127f2dc98fc1611db4f69cbae222e2e9e
Luis Carlos Rodriguez Barrios <luiscarrodba97@gmail.com>
2021-08-26 20:59:38 -0500

    password bug fixed

--- autenticacion_ms/usersApp/models.py
+++ autenticacion_ms/usersApp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-from django.contrib.auth.hashers import make_password
 
 
 class UserManager(BaseUserManager):
