cabe7e0ebfe939ed746047a12b1f0fc136035710
Camilo Navarro <camilo.navarro9812@gmail.com>
2021-09-01 09:33:06 -0500

    Base files Front

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,204 +1,60 @@
 <template>
-  <div id="app" class="app">
-    <div class="header">
-      <h1>Banco UN</h1>
-
-      <nav>
-        <button v-on:click="init" v-if="is_auth">Inicio</button>
-        <button v-on:click="account" v-if="is_auth">Mi Cuenta</button>
-        <button v-on:click="transacction" v-if="is_auth">Transacción</button>
-        <button v-on:click="historial" v-if="is_auth">Historial</button>
-        <button v-on:click="logOut" v-if="is_auth">Cerrar Sesión</button>
-      </nav>
-    </div>
-
-    <div class="main-component">
-      <router-view v-on:log-in="logIn"></router-view>
-    </div>
-
-    <div class="footer">
-      <h2>Misión TIC 2022</h2>
+<nav class="navbar navbar-light bg-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Offcanvas navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
     </div>
   </div>
+</nav>
 </template>
 
 <script>
-import gql from "graphql-tag";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 
 export default {
-  name: "App",
+  name: 'App',
+  components: {
+    
+  }
 
-  data: function () {
-    return {
-      is_auth: false,
-    };
-  },
-
-  created: function () {
-    this.updateAccessToken();
-  },
-
-  methods: {
-    updateAccessToken: async function () {
-      if (localStorage.getItem("refresh_token") == null) {
-        this.$router.push({ name: "user_auth" });
-        this.is_auth = false;
-        return;
-      }
-
-      await this.$apollo
-        .mutate({
-          mutation: gql`
-            mutation ($refreshTokenRefresh: String!) {
-              refreshToken(refresh: $refreshTokenRefresh) {
-                access
-              }
-            }
-          `,
-          variables: {
-            refreshTokenRefresh: localStorage.getItem("refresh_token"),
-          },
-        })
-        .then((result) => {
-          localStorage.setItem("access_token", result.data.refreshToken.access);
-          this.is_auth = true;
-        })
-        .catch((error) => {
-          alert("Su sesión expiró, vuelva a iniciar sesión.");
-          this.$router.push({ name: "user_auth" });
-          this.is_auth = false;
-          localStorage.clear();
-        });
-    },
-
-    logIn: async function (data, username) {
-      localStorage.setItem("access_token", data.access);
-      localStorage.setItem("refresh_token", data.refresh);
-      localStorage.setItem("user_id", data.user_id);
-      localStorage.setItem("current_username", username);
-
-      await this.updateAccessToken();
-      if (this.is_auth) this.init();
-    },
-
-    init: function () {
-      this.$router.push({
-        name: "user",
-        params: { username: localStorage.getItem("current_username") },
-      });
-    },
-
-    account: function () {
-      this.$router.push({
-        name: "account",
-        params: { username: localStorage.getItem("current_username") },
-      });
-    },
-
-    transacction: function () {
-      this.$router.push({
-        name: "transacction",
-        params: { username: localStorage.getItem("current_username") },
-      });
-    },
-
-    historial: function () {
-      this.$router.push({
-        name: "historial",
-        params: { username: localStorage.getItem("current_username") },
-      });
-    },
-    logOut: async function () {
-      localStorage.removeItem("access_token");
-      localStorage.removeItem("refresh_token");
-      localStorage.removeItem("user_id");
-      localStorage.removeItem("current_username");
-
-      await this.updateAccessToken();
-    },
-  },
-};
+}
 </script>
 
 <style>
-body {
-  margin: 0 0 0 0;
-}
 
-.header {
-  position: fixed;
-  left: 0px;
-  top: 0px;
-  width: 100%;
-  height: 100px;
-  margin: 0%;
-  padding: 0;
-
-  background-color: #283747;
-  color: #e5e7e9;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.header h1 {
-  width: 20%;
-  text-align: center;
-}
-
-.header nav {
-  height: 100%;
-  width: 40%;
-
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-
-  font-size: 20px;
-}
-
-.header nav button {
-  color: #e5e7e9;
-  background: #283747;
-  border: 1px solid #e5e7e9;
-
-  border-radius: 5px;
-  padding: 10px 20px;
-}
-
-.header nav button:hover {
-  color: #283747;
-  background: #e5e7e9;
-  border: 1px solid #e5e7e9;
-}
-
-.main-component {
-  height: 75vh;
-  margin: 0%;
-  padding: 0%;
-
-  background: #fdfefe;
-}
-
-.footer {
-  position: fixed;
-  left: 0px;
-  bottom: 0px;
-  width: 100%;
-  height: 75px;
-
-  background-color: #283747;
-  color: #e5e7e9;
-}
-
-.footer h2 {
-  margin: 0px;
-  width: 100%;
-  height: 100%;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 </style>
