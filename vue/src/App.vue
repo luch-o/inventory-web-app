9689967d01aa3c1c324790f9bb9eb80fa7cb9cf0
Luis Carlos Rodriguez Barrios <luiscarrodba97@gmail.com>
2021-09-02 09:13:43 -0500

    div wrapping app template

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,77 +1,94 @@
 <template>
-<nav class="navbar navbar-light bg-light fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">INVENTARIO FERRETERIA</a>
-    <button v-if="is_auth" class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menú</h5>
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">INICIO</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">PRODUCTOS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">VENTAS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" v-on:click="logOut" data-bs-dismiss="offcanvas" >CERRAR SESION</a>
-          </li>
-        </ul>
-          
+<div>
+  <nav class="navbar navbar-light bg-light fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">INVENTARIO FERRETERIA</a>
+      <button
+        v-if="is_auth"
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div
+        class="offcanvas offcanvas-end"
+        tabindex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menú</h5>
+          <button
+            type="button"
+            class="btn-close text-reset"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">INICIO</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">PRODUCTOS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">VENTAS</a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link"
+                href="#"
+                v-on:click="logOut"
+                data-bs-dismiss="offcanvas"
+                >CERRAR SESION</a
+              >
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-
-</nav>
+  </nav>
 
   <div class="main-component">
-      
-      <router-view v-on:log-in="logIn"></router-view>
-      
+    <router-view v-on:log-in="logIn"></router-view>
   </div>
-
-  
+</div>
 </template>
 
 <script>
-  import "bootstrap/dist/css/bootstrap.min.css"
-  import "bootstrap/dist/js/bootstrap"
-  import gql from "graphql-tag";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import gql from "graphql-tag";
 
-  export default {
-    name: "App",
+export default {
+  name: "App",
 
-    data: function () {
-      return {
-        is_auth: false,
-      };
-    },
+  data: function () {
+    return {
+      is_auth: false,
+    };
+  },
 
-    created: function () {
+  created: function () {
+    if (this.is_auth == true) {
+      this.updateAccessToken();
+    }
+  },
 
-      if(this.is_auth == true){
-        this.updateAccessToken();
+  methods: {
+    //Preguntar registro, porque no pinta el registro, error await en el apollo
+    updateAccessToken: async function () {
+      if (localStorage.getItem("refresh_token") == null) {
+        this.$router.push({ name: "login" });
+        this.is_auth = false;
+        return;
       }
-      
-    },
-
-    methods:{
-      //Preguntar registro, porque no pinta el registro, error await en el apollo
-      updateAccessToken: async function () {
-        if (localStorage.getItem("refresh_token") == null) {
-          this.$router.push({ name: "login" });
-          this.is_auth = false;
-          return;
-        }
-        await this.$apollo
+      await this.$apollo
         .mutate({
           mutation: gql`
             mutation RefreshTokenMutation($refreshTokenRefresh: String) {
@@ -94,50 +111,43 @@
           this.is_auth = false;
           localStorage.clear();
         });
-        
-      },
-
-      logIn: async function (data, username) {
-        
-        localStorage.setItem("access_token", data.access);
-        localStorage.setItem("refresh_token", data.refresh);
-        localStorage.setItem("user_id", data.user_id);
-        localStorage.setItem("current_username", username);
-
-        await this.updateAccessToken();
-        if (this.is_auth){
-          this.home();
-        } 
-      },
-
-      home: function () {
-        this.$router.push({
-          name: "home",
-          params: { username: localStorage.getItem("current_username") },
-        });
-      },
-      logOut: async function () {
-        localStorage.removeItem("access_token");
-        localStorage.removeItem("refresh_token");
-        localStorage.removeItem("user_id");
-        localStorage.removeItem("current_username");
-        alert("Se ha cerrado sesion exitosamente");
-        await this.updateAccessToken();
-      },
-
-      
-
-
     },
-  }
+
+    logIn: async function (data, username) {
+      localStorage.setItem("access_token", data.access);
+      localStorage.setItem("refresh_token", data.refresh);
+      localStorage.setItem("user_id", data.user_id);
+      localStorage.setItem("current_username", username);
+
+      await this.updateAccessToken();
+      if (this.is_auth) {
+        this.home();
+      }
+    },
+
+    home: function () {
+      this.$router.push({
+        name: "home",
+        params: { username: localStorage.getItem("current_username") },
+      });
+    },
+    logOut: async function () {
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("refresh_token");
+      localStorage.removeItem("user_id");
+      localStorage.removeItem("current_username");
+      alert("Se ha cerrado sesion exitosamente");
+      await this.updateAccessToken();
+    },
+  },
+};
 </script>
 
 <style>
-  *{
-    background: white;
-  }
-  p{
-    margin: 100px;
-  }
-  
+* {
+  background: white;
+}
+p {
+  margin: 100px;
+}
 </style>
